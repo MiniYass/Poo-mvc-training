2e4fad7441a31cb2f6d3b5a8e9d77c709ba91ae8
Sekri Yassine <58735130+MiniYass@users.noreply.github.com>
2020-05-08 13:06:25 +0200

    Update bdclass.php

--- bdclass.php
+++ bdclass.php
@@ -3,6 +3,7 @@
     class Dbconnect{
 
         static public function connection(){
+            // ici la fonction est en static pour pouvoir être utilisé plus facilement sur mes differente pages.
 
             try{
                $dns="mysql:host=localhost;dbname=clients;charset=utf8";
